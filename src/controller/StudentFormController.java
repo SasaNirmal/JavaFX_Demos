2aa030e426ea580e02809efccde50731d3264edb
Sasanka Nirmal <sasanpathirage@gmail.com>
2025-09-11 23:57:20 +0530

    inisial Commit

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,4 +1,9 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class StudentFormController {
+    public void btnHelloWorldOnAction(ActionEvent actionEvent) {
+        System.out.println("Hello, World!");
+    }
 }

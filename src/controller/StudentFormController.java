f5331e463ad38c43489379a9b4e29576a911f0f7
Sasanka Nirmal <sasanpathirage@gmail.com>
2025-09-13 00:02:52 +0530

    Create UI with Javafx an Scene Builder

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,9 +1,37 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class StudentFormController {
+    public TextField txtStudentName;
+    public TextField txtStudentAddress;
+    public Label lblMassage;
+
+
+    public void btnPrintStudentName(ActionEvent actionEvent) {
+        String studentName = txtStudentName.getText();
+
+        System.out.println(studentName);
+
+        txtStudentName.clear();
+        txtStudentName.requestFocus();
+    }
+
+    public void btnPrintStudentAdress(ActionEvent actionEvent) {
+        String stidentAddress = txtStudentAddress.getText();
+
+        System.out.println(stidentAddress);
+        txtStudentAddress.clear();
+        txtStudentName.requestFocus();
+    }
+
+
     public void btnHelloWorldOnAction(ActionEvent actionEvent) {
-        System.out.println("Hello, World!");
+        String massage = "Hello "+txtStudentName.getText()+"!,Your Address is "+txtStudentAddress.getText()+".";
+        System.out.println(massage);
+        lblMassage.setText(massage);
+
     }
 }

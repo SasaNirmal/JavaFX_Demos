f231905c99b5ba70ec13b95cc352134170847e7d
Sasanka Nirmal <sasanpathirage@gmail.com>
2025-09-12 10:43:16 +0530

    create the Main method

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppInitializer extends Application {
 
@@ -9,7 +14,13 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        Parent parent = FXMLLoader.load(this.getClass().getResource("/view/StudentForm.fxml"));//load the fxml file
+        Scene scene = new Scene(parent);//create a scene
 
+        primaryStage.setScene(scene);//set the scene to the stage
+        primaryStage.setTitle("Student Form");
+        primaryStage.centerOnScreen();
+        primaryStage.show();//show the stage
     }
 }

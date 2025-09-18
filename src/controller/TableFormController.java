346a80996506257a0f27f4d7ae7e08cc6f8c06af
Sasanka Nirmal <sasanpathirage@gmail.com>
2025-09-19 02:04:11 +0530

    Create a anonemous methos for set datat again text feild

--- src/controller/TableFormController.java
+++ src/controller/TableFormController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
@@ -29,8 +31,41 @@ public class TableFormController {
         tblCountermenDetails.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("Address"));
         tblCountermenDetails.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("NIC"));
         tblCountermenDetails.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("Contact"));
+
+        loadStartCoustermerData();
+
+        tblCountermenDetails.refresh();
+
+        tblCountermenDetails.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Coustmer_tm>() {
+            @Override
+            public void changed(ObservableValue<? extends Coustmer_tm> observableValue, Coustmer_tm coustmerTm, Coustmer_tm t1) {
+
+                Coustmer_tm selectedItem = tblCountermenDetails.getSelectionModel().getSelectedItem();
+
+                txtID.setText(selectedItem.getID());
+                txtNM.setText(selectedItem.getName());
+                txtAds.setText(selectedItem.getAddress());
+                txtNIC.setText(selectedItem.getNIC());
+                txtCntt.setText(selectedItem.getContact());
+            }
+        });
     }
 
+    public void loadStartCoustermerData(){
+        Coustmer_tm coustermer1 = new Coustmer_tm("C001","Kasun","Galle","123456789V","0771234567");
+        Coustmer_tm coustermer2 = new Coustmer_tm("C002","Nimal","Matara","987654321V","0717654321");
+        Coustmer_tm coustermer3 = new Coustmer_tm("C003","Sunil","Colombo","456789123V","0759876543");
+        Coustmer_tm coustermer4 = new Coustmer_tm("C004","Kamal","Kandy","321654987V","0723456789");
+        Coustmer_tm coustermer5 = new Coustmer_tm("C005","Ruwan","Jaffna","654321789V","0788765432");
+
+        ObservableList<Coustmer_tm> items = tblCountermenDetails.getItems();
+
+        items.add(coustermer1);
+        items.add(coustermer2);
+        items.add(coustermer3);
+        items.add(coustermer4);
+        items.add(coustermer5);
+    }
 
     public void btnCoustmerAddOnAction(ActionEvent actionEvent) {
 
